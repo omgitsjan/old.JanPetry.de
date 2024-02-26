a7f88a6bc91246817bd978cd99e6a609253ba7b8
Jan Petry <42674570+omgitsjan@users.noreply.github.com>
2024-02-26 17:32:44 +0100

    Update NewsletterAlert.vue

--- components/NewsletterAlert.vue
+++ components/NewsletterAlert.vue
@@ -1,19 +1,24 @@
+<script setup lang="ts">
+const { prev } = useContent()
+</script>
 <template>
   <div class="max-w-xl relative border transition rounded-xl overflow-hidden dark:border-gray-500">
     <div class="p-4">
       <h3 class="font-semibold mb-3">
-        Meet up
+        Newsletter
       </h3>
       <div class="text-sm mt-1 text-gray-500 dark:text-gray-300 prose">
         <p class="mb-2">
-          I'm back in Sydney for a couple of months while working on open-source.
+          I'm backpacking Europe while working on open-source. I'm currently in <Location class="inline" />, let's <NuxtLink to="/meet" class="link">
+            meet up
+          </NuxtLink>!
         </p>
-        <p>
-          In the area and want to catch up? <NuxtLink to="/meet" class="link">Get in touch</NuxtLink>!
+        <p class="mb-2">
+          Previously:  <NuxtLink :to="prev._path" class="link">
+            {{ prev.title }}
+          </Nuxtlink>.
         </p>
       </div>
     </div>
   </div>
 </template>
-<script setup lang="ts">
-</script>
